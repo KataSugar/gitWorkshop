914955b149c5fae55e5bc42b674f04d75959b649
KataSugar <159426291+KataSugar@users.noreply.github.com>
2024-07-30 11:53:00 +0200

    add task1

--- 1/showLast.js
+++ 1/showLast.js
@@ -4,6 +4,10 @@ const getBeers = require('../data')
 
 function getLastBeer() {
     // TODO: return the last beer object (use `getBeers` for the solution)
+    console.log(getBeers().length)
+    console.log(getBeers().length-1)
+    return getBeers()[getBeers().length-1]
+    
 }
 
 if (getLastBeer()?.name === "All the Leaves are Brown") {
